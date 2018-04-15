824ed8d8cf6c855374d3d11bfbffc4c33292be14
Mark Mikofski <bwanamarko@gmail.com>
2018-04-15 04:55:24 +0200

    make relative urls in settingss

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,8 +16,8 @@ TIMEZONE = 'US/Pacific'
 DEFAULT_LANG = u'English'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
@@ -51,4 +51,4 @@ COPYRIGHT_YEAR = 2017
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
