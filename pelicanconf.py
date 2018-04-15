9212d9e171c4c014f9bddf89b5a881003a1772fe
Mark Mikofski <bwana.marko@yahoo.com>
2018-04-14 21:36:11 -0700

    no rss/atom feeds for dev site

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,8 +16,8 @@ TIMEZONE = 'US/Pacific'
 DEFAULT_LANG = u'English'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
